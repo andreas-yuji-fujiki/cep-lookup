85d9bb1ab7db1d10e870f5f09062d2155efad274
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-10-08 16:30:43 -0300

    fixed some mess in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: "/cep-lookup"
-=======
   base: "/cep-lookup",
->>>>>>> 38e9b8d (added the base url to vite config)
+  base: "/cep-lookup",
 })
